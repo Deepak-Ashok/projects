1e2a6d1e0642a4a155043ed0cc5bdb9afe6e8044
deepakasd <54941221+deepakasd@users.noreply.github.com>
2020-02-13 23:54:28 +0530

    Create encryptsimple.c

---
+++ encryptsimple.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() 
+{
+	char ch;
+        int numAscii;
+        FILE * fptr, * fptr2;
+        
+        
+        fptr = fopen("tvt.txt", "w");// "w" defines "writing mode"
+        fptr2= fopen("deep.txt","r");
+       
+         ch=fgetc(fptr2);
+         while(ch!=EOF){
+			numAscii=(int)ch;
+			numAscii=numAscii+5;
+			
+			fprintf(fptr,"%c",numAscii);
+			ch=fgetc(fptr2);
+		}
+        fclose(fptr2);
+        fclose(fptr);
+        return 0;
+    }
